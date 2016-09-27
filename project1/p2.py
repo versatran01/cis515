911b3b6f5b9c993c512f15f689c3168c3f1c83d4
Chao Qu <quchao@seas.upenn.edu>
2016-09-27 19:41:16 -0400

    add t to toggle marker

--- project1/p2.py
+++ project1/p2.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
@@ -5,31 +6,59 @@ from project1.bezier import (BezierBase, BezierBernstein, BezierDeCasteljau,
                              BezierSubdivision)
 
 
+class BezierType(Enum):
+    Bernstein = 1
+    DeCasteljau = 2
+    Subdivision = 3
+
+
 class BezierBuilder:
-    def __init__(self, control_points, bezier):
+    def __init__(self, ax):
         """
-        :param control_points:
-        :type control_points: Line2D
-        :param bezier:
-        :type bezier: BezierBase
+        Key-bindings
+
+            't' toggle vertex markers on and off.
         """
-        self.control_points = control_points
-        self.xp = list(control_points.get_xdata())
-        self.yp = list(control_points.get_ydata())
-        self.canvas = control_points.figure.canvas
-        self.ax = control_points.axes
+        self.marker_on = True
+
+        self.ax = ax
+        control_points = Line2D([], [],
+                                linestyle='--', marker='+',
+                                markeredgewidth=2)
+        self.line_control_points = self.ax.add_line(control_points)
+        self.control_points_x = self.line_control_points.get_xdata()
+        self.control_points_y = self.line_control_points.get_ydata()
+        self.canvas = self.line_control_points.figure.canvas
 
         # Event handler for mouse clicking
-        self.cid_click = self.canvas.mpl_connect('button_press_event', self)
+        self.cid_button_press = self.canvas.mpl_connect('button_press_event',
+                                                        self.on_button_press)
+        self.cid_key_press = self.canvas.mpl_connect('key_press_event',
+                                                     self.on_key_press)
 
         # Create Bezier curve
-        line_bezier = Line2D([], [],
-                             color=control_points.get_markeredgecolor())
-        self.bezier_curve = self.ax.add_line(line_bezier)
+        bezier_curve = Line2D([], [],
+                              color=self.line_control_points.get_markeredgecolor())
+        self.line_bezier = self.ax.add_line(bezier_curve)
 
-        self.bezier = bezier
+        self.bezier = BezierSubdivision()
 
-    def __call__(self, event):
+    def on_key_press(self, event):
+        """
+        :param event:
+        :return:
+        """
+
+        if event.key == 't':
+            self.marker_on = not self.marker_on
+            if self.marker_on:
+                self.line_bezier.set_marker('.')
+            else:
+                self.line_bezier.set_marker("")
+
+        self.canvas.draw()
+
+    def on_button_press(self, event):
         """
         :param event:
         :return:
@@ -39,16 +68,18 @@ class BezierBuilder:
             return
 
         # Add control point
-        self.xp.append(event.xdata)
-        self.yp.append(event.ydata)
-        self.control_points.set_data(self.xp, self.yp)
+        self.control_points_x.append(event.xdata)
+        self.control_points_y.append(event.ydata)
+        self.line_control_points.set_data(self.control_points_x,
+                                          self.control_points_y)
 
         # Rebuild Bezier curve and update canvas
-        self.bezier_curve.set_data(*self.build_bezier())
+        self.line_bezier.set_data(*self.build_bezier())
         self.canvas.draw()
 
     def build_bezier(self):
-        points = self.bezier.create_curve(list(zip(self.xp, self.yp)))
+        points = self.bezier.create_curve(
+            list(zip(self.control_points_x, self.control_points_y)))
         x, y = np.transpose(points)
         return x, y
 
@@ -57,14 +88,7 @@ if __name__ == '__main__':
     # Initial setup
     fig, ax = plt.subplots()
 
-    line = Line2D([], [],
-                  linestyle='--', marker='+', markeredgewidth=2)
-    ax.add_line(line)
-
-    bezier = BezierSubdivision()
-    # bezier = BezierDeCasteljau()
-    # bezier = BezierBernstein()
     # Create BezierBuilder
-    bezier_builder = BezierBuilder(line, bezier)
+    bezier_builder = BezierBuilder(ax)
 
     plt.show()
