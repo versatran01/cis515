881d03920fd16ac36d64aed0c2f325cdbdbdd395
chao <quchao@seas.upenn.edu>
2016-10-14 19:45:03 -0400

    add haar.py

---
+++ project2/haar.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def plf(u):
+    return u
+
+
+def haar(u):
+    return u
+
+
+def haar_inv(v):
+    return v
+
+
+def haar_step(u, k):
+    return u
+
+
+def haar_inv_step(v, k):
+    return v
+
+
