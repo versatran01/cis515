aa4355352793fab72182a3b3ec5c661595b9f0ee
chao <versatran01@gmail.com>
2016-11-17 10:35:34 -0500

    add bessel

--- project3/scripts/end_cond.py
+++ project3/scripts/end_cond.py
@@ -24,9 +24,8 @@ def curve_interp(points, end_cond):
 
 def make_deboor(X, d, d0, dN):
     n, c = np.shape(X)
-    N = n - 1
 
-    D = np.empty((N + 3, c))
+    D = np.empty((n + 2, c))
     D[0] = X[0]
     D[1] = d0
     D[2:-2] = d
@@ -36,14 +35,26 @@ def make_deboor(X, d, d0, dN):
     return D
 
 
+def make_interp_lhs(m, c=None):
+    """
+    Create lhs of interpolation system
+    :param m: size of the system
+    :param c: A[0,0] = A[-1,-1] = c
+    :return:
+    """
+    u = np.ones(m - 1)
+    A = tridiag_from_udl(u, np.ones(m) * 4, u)
+    if c is not None:
+        A[0, 0] = c
+        A[-1, -1] = c
+    return A
+
+
 def natural_end_cond(X):
     n, c = np.shape(X)
-    N = n - 1  # number of Bezier curves
-    a = N - 1  # size of A, min is 2
 
     # Assemble system
-    u = np.ones(a - 1)
-    A = tridiag_from_udl(u, np.ones(a) * 4, u)
+    A = make_interp_lhs(n - 2)
     B = np.multiply(X[1:-1], 6)
     B[0] -= X[0]
     B[-1] -= X[-1]
@@ -61,15 +72,9 @@ def natural_end_cond(X):
 
 def quadratic_end_cond(X):
     n, c = np.shape(X)
-    N = n - 1  # number of Bezier curves
-    a = N - 1  # size of A, min is 2
 
     # Assemble system
-    u = np.ones(a - 1)
-    A = tridiag_from_udl(u, np.ones(a) * 4, u)
-    A[0, 0] = 5
-    A[-1, -1] = 5
-
+    A = make_interp_lhs(n - 2, 5)
     B = np.multiply(X[1:-1], 6)
     B[0] += X[1] - X[0]
     B[-1] += X[-2] - X[-1]
@@ -83,20 +88,34 @@ def quadratic_end_cond(X):
     return D
 
 
-def bessel_end_cond():
-    pass
+def bessel_end_cond(X):
+    n, c = np.shape(X)
+
+    # Assemble system
+    A = make_interp_lhs(n - 2, 7.0 / 2)
+    d0 = 1.0 / 2 * X[0] + 2.0 / 3 * X[1] - 1.0 / 6 * X[2]
+    dN = -1.0 / 6 * X[-3] + 2.0 / 3 * X[-2] + 1.0 / 2 * X[-1]
+
+    B = np.multiply(X[1:-1], 6)
+    B[0] -= 3.0 / 2 * d0
+    B[-1] -= 3.0 / 2 * dN
+
+    d = lu_solve_tridiag(A, B, True)
+    D = make_deboor(X, d, d0, dN)
+    return D
 
 
 def knot_end_cond():
     pass
 
 
-points_x = [0, 0, 1, 1, 2]
-points_y = [0, 1, 1, 0, 1]
-s = 5
+points_x = [0, 0, 1, 1, 2, 2]
+points_y = [0, 1, 1, 0, 1, 0]
+s = 6
 
 X = np.vstack((points_x[:s], points_y[:s])).T
-D = quadratic_end_cond(X)
+X = np.array(X, float)
+D = natural_end_cond(X)
 Dx, Dy = D.T
 
 fig, ax = plt.subplots()
