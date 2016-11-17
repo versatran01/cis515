f4f571369ca3e5e0e4d544719bda218bc79142eb
chao <versatran01@gmail.com>
2016-11-17 01:06:01 -0500

    add quadratic

--- project3/scripts/end_cond.py
+++ project3/scripts/end_cond.py
@@ -22,6 +22,20 @@ def curve_interp(points, end_cond):
     return interp_fun(points)
 
 
+def make_deboor(X, d, d0, dN):
+    n, c = np.shape(X)
+    N = n - 1
+
+    D = np.empty((N + 3, c))
+    D[0] = X[0]
+    D[1] = d0
+    D[2:-2] = d
+    D[-2] = dN
+    D[-1] = X[-1]
+
+    return D
+
+
 def natural_end_cond(X):
     n, c = np.shape(X)
     N = n - 1  # number of Bezier curves
@@ -40,18 +54,33 @@ def natural_end_cond(X):
     dN = 1.0 / 3 * d[-1] + 2.0 / 3 * X[-1]
 
     # Assemble all deBoor control points
-    D = np.empty((N + 3, c))
-    D[0] = X[0]
-    D[1] = d0
-    D[2:-2] = d
-    D[-2] = dN
-    D[-1] = X[-1]
+    D = make_deboor(X, d, d0, dN)
 
     return D
 
 
-def quadratic_end_cond():
-    pass
+def quadratic_end_cond(X):
+    n, c = np.shape(X)
+    N = n - 1  # number of Bezier curves
+    a = N - 1  # size of A, min is 2
+
+    # Assemble system
+    u = np.ones(a - 1)
+    A = tridiag_from_udl(u, np.ones(a) * 4, u)
+    A[0, 0] = 5
+    A[-1, -1] = 5
+
+    B = np.multiply(X[1:-1], 6)
+    B[0] += X[1] - X[0]
+    B[-1] += X[-2] - X[-1]
+
+    # Solve for deBoor control points
+    d = lu_solve_tridiag(A, B, True)
+    d0 = d[0] + 2.0 / 3 * X[0] - 2.0 / 3 * X[1]
+    dN = d[-1] + 2.0 / 3 * X[-1] - 2.0 / 3 * X[-2]
+
+    D = make_deboor(X, d, d0, dN)
+    return D
 
 
 def bessel_end_cond():
@@ -62,12 +91,12 @@ def knot_end_cond():
     pass
 
 
-points_x = [0, 1, 1, 2, 2]
-points_y = [0, 2, 0, 2, 0]
-s = 4
+points_x = [0, 0, 1, 1, 2]
+points_y = [0, 1, 1, 0, 1]
+s = 5
 
 X = np.vstack((points_x[:s], points_y[:s])).T
-D = natural_end_cond(X)
+D = quadratic_end_cond(X)
 Dx, Dy = D.T
 
 fig, ax = plt.subplots()
