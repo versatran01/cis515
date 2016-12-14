003a890bb6c93158fa8c2be5f731d6f5b193c440
Casey Kent <ckent@seas00601.wlan.wireless-pennnet.upenn.edu>
2016-12-14 12:57:40 -0500

    sim3 stuff

--- project4/sim3.py
+++ project4/sim3.py
@@ -17,6 +17,15 @@ def sim3_exp_SIM3(sim3):
     :param sim3: 4x4 matrix
     :return: SIM3, 4x4 matrix
     """
+    r7 = vee_sim3_R7(sim3)  # get r7 representation
+    lmda = r7[0]
+    theta = (r7[1]**2 + r7[2]**2 + r7[3]**2)**0.5  # compute theta
+
+    if lmda != 0 and theta  == 0:
+
+
+
+
     pass
 
 
@@ -26,6 +35,12 @@ def hat_R7_sim3(R7):
     :param R7: 1x7 vector
     :return: sim3, 4x4 matrix
     """
+    A = np.array([[R7[0], -R7[3], R7[2], R7[5]],
+                 [R7[3], R7[0], -R7[1], R7[6]],
+                 [-R7[2], R7[1], R7[0], R7[7]],
+                 [0, 0, 0, 0]], np.float)
+
+    return A
     pass
 
 
@@ -35,4 +50,8 @@ def vee_sim3_R7(sim3):
     :param sim3: 4x4 matrix
     :return: R7, 1x7 vector
     """
+    v = np.array([sim3[0, 0], sim3[2, 1], sim3[0, 2], sim3[1, 0], sim3[0, 3], sim3[1, 3], sim3[2, 3]], np.float)
+
+    return v
+
     pass
